6f7db541308b77fdab1f2c6cab3d918525f86541
Matheus Santana <32546688+santanma@users.noreply.github.com>
2023-07-07 17:52:13 -0300

    Update lista.h

--- lista.h
+++ lista.h
@@ -1,8 +1,16 @@
 #include <stdbool.h>
+#include "fila.h"
 
 typedef struct lista Lista;
 typedef struct noMemoria NoMemoria;
 
-Lista* criarLista();
+Lista* criarListaCPU(int tamanhoCPU);
 
-bool listaVazia(Lista *l);
+bool listaCPUVazia(Lista *listaCPU);
+
+NoMemoria* localizarParticaoExecutandoNaCPU(Lista *listaCPU);
+
+void insereListaCPUOrdenada(Lista *listaCPU,NoMemoria *particaoNova,NoMemoria *particaoCorrente);
+
+void alocarMemoriaBestFit(Lista *listaCPU,NoProcesso *processo);
+
