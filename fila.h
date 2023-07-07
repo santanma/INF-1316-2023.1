4a5366faed0e5c4c22c4a9fbfcb4afb51efcb42b
Matheus Santana <32546688+santanma@users.noreply.github.com>
2023-07-06 23:25:53 -0300

    Update fila.h

--- fila.h
+++ fila.h
@@ -1,13 +1,21 @@
+#include <stdbool.h> 
+
 typedef struct tipoInformacao TipoInformacao;
 typedef struct noProcesso NoProcesso;
 typedef struct fila Fila;
 
 Fila* criarFila ();
+
 void inserirFila (Fila *f,
 				  int idProcesso,
 				  int tamanhoProcesso,
 				  int quantidadeExecIo,
 				  char *tipoExecIo,
 				  int tempoExecIo);
+
 void imprimirFila(Fila *f);
+ 
+bool filaVazia(Fila *f);
+
+NoProcesso* retirarProcessoDaFila(Fila *f);
  
